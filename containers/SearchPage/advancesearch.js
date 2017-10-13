8164d8fd3749b95c50925599897e6dc85d8c657b
Arthur Sudre <arthursudre@gmail.com>
2017-10-14 00:08:51 +0200

    chore(babel): use preset env

--- containers/SearchPage/advancesearch.js
+++ containers/SearchPage/advancesearch.js
@@ -5,16 +5,17 @@ import Divider from 'material-ui/Divider';
 import SelectField from 'material-ui/SelectField';
 import MenuItem from 'material-ui/MenuItem';
 
-
 const status = ["Maître","Gradué","Étudiant","Suppôt","Extérieur","Incertain"];
 const grade = ["Magister","Docteur","Maître","Licencié","Bachelier","Étudiant"];
 
-
 class AdvanceSearch extends Component {
-  state = {
-    "status":1,
-    "grade":1,
-    "discipline":1
+  constructor() {
+    super();
+    this.state = {
+      "status":1,
+      "grade":1,
+      "discipline":1
+    }
   }
 
   handleStatusChange = (event, index, value) => this.setState({status:value});
@@ -23,40 +24,40 @@ class AdvanceSearch extends Component {
 
   render() {
     return (
-    <div>
-      <h1>Recherche avancée</h1>
-      <p>
-      Une partie du nom : <TextField />
-      <Divider />
-      Médiane d'activité
-      Ou
-      Début et Fin d'activité
-      <Divider />
-      <h4>Cursus</h4>
-      <SelectField floatingLabelText="Status" value={this.state.status} onChange={this.handleStatusChange}>
-          <MenuItem value={0} primaryText="Never" />
-          <MenuItem value={1}  primaryText="Every Night" />
-          <MenuItem value={2}  primaryText="Weeknights" />
-      </SelectField>
-      <SelectField floatingLabelText="Grade obtenu" value={this.state.grade} onChange={this.handleGradeChange}>
-          <MenuItem value={0} primaryText="Never" />
-          <MenuItem value={1}  primaryText="Every Night" />
-          <MenuItem value={2}  primaryText="Weeknights" />
-      </SelectField>
-      <SelectField floatingLabelText="Discipline" value={this.state.discipline} onChange={this.handleDisciplineChange}>
-          <MenuItem value={0} primaryText="Never" />
-          <MenuItem value={1}  primaryText="Every Night" />
-          <MenuItem value={2}  primaryText="Weeknights" />
-      </SelectField>
-      <Divider />
-      Biographie :
-      <Divider />
-      Production textuelle :
-      <Divider />
-      Bibliographie :
-      <Divider />
-      </p>
-    </div>
+      <div>
+        <h1>Recherche avancée</h1>
+        <p>
+          Une partie du nom : <TextField />
+          <Divider />
+          Médiane d'activité
+          Ou
+          Début et Fin d'activité
+          <Divider />
+          <h4>Cursus</h4>
+          <SelectField floatingLabelText="Status" value={this.state.status} onChange={this.handleStatusChange}>
+            <MenuItem value={0} primaryText="Never" />
+            <MenuItem value={1}  primaryText="Every Night" />
+            <MenuItem value={2}  primaryText="Weeknights" />
+          </SelectField>
+          <SelectField floatingLabelText="Grade obtenu" value={this.state.grade} onChange={this.handleGradeChange}>
+            <MenuItem value={0} primaryText="Never" />
+            <MenuItem value={1}  primaryText="Every Night" />
+            <MenuItem value={2}  primaryText="Weeknights" />
+          </SelectField>
+          <SelectField floatingLabelText="Discipline" value={this.state.discipline} onChange={this.handleDisciplineChange}>
+            <MenuItem value={0} primaryText="Never" />
+            <MenuItem value={1}  primaryText="Every Night" />
+            <MenuItem value={2}  primaryText="Weeknights" />
+          </SelectField>
+          <Divider />
+          Biographie :
+          <Divider />
+          Production textuelle :
+          <Divider />
+          Bibliographie :
+          <Divider />
+        </p>
+      </div>
     );
   }
 }
--- containers/SearchPage/index.js
+++ containers/SearchPage/index.js
@@ -9,70 +9,90 @@ import Toggle from 'material-ui/Toggle';
 import SearchResult from '../../components/SearchResult';
 import AdvanceSearch from './advancesearch';
 
-const results = [{
-    "id":"11546",
-    "name":"ABRAHAM Alberti",
-    "mediane":"1435",
-    "status":"Maitre",
-    "origine":"France"
+const results = [
+  {
+    id: '11546',
+    name: 'ABRAHAM Alberti',
+    mediane: '1435',
+    status: 'Maitre',
+    origine: 'France'
   },
   {
-    "id":"16792",
-    "name":"ABSALON Absolonis",
-    "mediane":"1256",
-    "status":"Etudiant",
-    "origine":"Italie"
+    id: '16792',
+    name: 'ABSALON Absolonis',
+    mediane: '1256',
+    status: 'Etudiant',
+    origine: 'Italie'
   },
   {
-    "id":"8925",
-    "name":"ADAM Barate",
-    "mediane":"1256",
-    "status":"Etudiant",
-    "origine":"Suède"
+    id: '8925',
+    name: 'ADAM Barate',
+    mediane: '1256',
+    status: 'Etudiant',
+    origine: 'Suède'
   }
 ];
 
-const status = ["Maître","Gradué","Étudiant","Suppôt","Extérieur","Incertain"];
-const grade = ["Magister","Docteur","Maître","Licencié","Bachelier","Étudiant"];
-
+const status = [
+  'Maître',
+  'Gradué',
+  'Étudiant',
+  'Suppôt',
+  'Extérieur',
+  'Incertain'
+];
+const grade = [
+  'Magister',
+  'Docteur',
+  'Maître',
+  'Licencié',
+  'Bachelier',
+  'Étudiant'
+];
 
 class SearchPage extends Component {
-  state = {
-    "status":1,
-    "grade":1,
-    "discipline":1,
-    "advancesearch":false,
-    activateCompleteSearchFeature:false
+  constructor() {
+    super();
+    this.state = {
+      status: 1,
+      grade: 1,
+      discipline: 1,
+      advancesearch: false,
+      activateCompleteSearchFeature: false
+    };
   }
 
-  handleStatusChange = (event, index, value) => this.setState({status:value});
-  handleGradeChange = (event, index, value) => this.setState({grade:value});
-  handleDisciplineChange = (event, index, value) => this.setState({discipline:value});
-
   handleToogleChange = (event, index, value) => {
-console.log(index);
-console.log(value);
-console.log(this.state.advancesearch);
-    this.setState({advancesearch:!this.state.advancesearch})};
+    console.log(index);
+    console.log(value);
+    console.log(this.state.advancesearch);
+    this.setState({ advancesearch: !this.state.advancesearch });
+  };
 
   render() {
     return (
-    <div>
-      <h1>Rechercher une fiche</h1>
-      <TextField floatingLabelText="Saisissez un mot clé" className="app-search-field"/>
-      <RaisedButton label="Rechercher" primary={true} />
-      {this.state.activateCompleteSearchFeature ?
-        <div>
-          <div className="app-search-toggle">
-            <Toggle label="Recherche avancée" value={this.state.advancesearch}  onClick={this.handleToogleChange}/>
+      <div>
+        <h1>Rechercher une fiche</h1>
+        <TextField
+          floatingLabelText="Saisissez un mot clé"
+          className="app-search-field"
+        />
+        <RaisedButton label="Rechercher" primary={true} />
+        {this.state.activateCompleteSearchFeature ? (
+          <div>
+            <div className="app-search-toggle">
+              <Toggle
+                label="Recherche avancée"
+                value={this.state.advancesearch}
+                onClick={this.handleToogleChange}
+              />
+            </div>
+            {this.state.advancesearch ? <AdvanceSearch /> : ''}
           </div>
-          {this.state.advancesearch ? <AdvanceSearch />:''}
+        ) : null}
+        <h1>Résultats</h1>
+        <SearchResult data={results} />
       </div>
-      : null
-      }
-      <h1>Résultats</h1>
-      <SearchResult data={results}/>
-    </div>
     );
   }
 }
