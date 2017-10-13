09a97642c5a2acf7a0fb8e5a1ba7c250a4c87f60
Arthur Sudre <arthursudre@gmail.com>
2017-10-14 00:46:43 +0200

    chore(flow): add flowtype

--- containers/SearchPage/advancesearch.js
+++ containers/SearchPage/advancesearch.js
@@ -1,3 +1,5 @@
+// @flow
+
 import React, { PropTypes, Component } from 'react';
 import TextField from 'material-ui/TextField';
 import RaisedButton from 'material-ui/RaisedButton';
@@ -5,22 +7,48 @@ import Divider from 'material-ui/Divider';
 import SelectField from 'material-ui/SelectField';
 import MenuItem from 'material-ui/MenuItem';
 
-const status = ["Maître","Gradué","Étudiant","Suppôt","Extérieur","Incertain"];
-const grade = ["Magister","Docteur","Maître","Licencié","Bachelier","Étudiant"];
+const status = [
+  'Maître',
+  'Gradué',
+  'Étudiant',
+  'Suppôt',
+  'Extérieur',
+  'Incertain'
+];
+const grade = [
+  'Magister',
+  'Docteur',
+  'Maître',
+  'Licencié',
+  'Bachelier',
+  'Étudiant'
+];
 
-class AdvanceSearch extends Component {
+type State = {
+  status: number,
+  grade: number,
+  discipline: number
+};
+
+class AdvanceSearch extends Component<void, State> {
   constructor() {
     super();
     this.state = {
-      "status":1,
-      "grade":1,
-      "discipline":1
-    }
+      status: 1,
+      grade: 1,
+      discipline: 1
+    };
   }
 
-  handleStatusChange = (event, index, value) => this.setState({status:value});
-  handleGradeChange = (event, index, value) => this.setState({grade:value});
-  handleDisciplineChange = (event, index, value) => this.setState({discipline:value});
+  handleChange = (stateKey: string) => (
+    event: Event,
+    index: number,
+    value: number
+  ) => this.setState({ [stateKey]: value });
+
+  handleStatusChange = this.handleChange('status');
+  handleGradeChange = this.handleChange('grade');
+  handleDisciplineChange = this.handleChange('discipline');
 
   render() {
     return (
@@ -29,25 +57,35 @@ class AdvanceSearch extends Component {
         <p>
           Une partie du nom : <TextField />
           <Divider />
-          Médiane d'activité
-          Ou
-          Début et Fin d'activité
+          Médiane d'activité Ou Début et Fin d'activité
           <Divider />
           <h4>Cursus</h4>
-          <SelectField floatingLabelText="Status" value={this.state.status} onChange={this.handleStatusChange}>
+          <SelectField
+            floatingLabelText="Status"
+            value={this.state.status}
+            onChange={this.handleStatusChange}
+          >
             <MenuItem value={0} primaryText="Never" />
-            <MenuItem value={1}  primaryText="Every Night" />
-            <MenuItem value={2}  primaryText="Weeknights" />
+            <MenuItem value={1} primaryText="Every Night" />
+            <MenuItem value={2} primaryText="Weeknights" />
           </SelectField>
-          <SelectField floatingLabelText="Grade obtenu" value={this.state.grade} onChange={this.handleGradeChange}>
+          <SelectField
+            floatingLabelText="Grade obtenu"
+            value={this.state.grade}
+            onChange={this.handleGradeChange}
+          >
             <MenuItem value={0} primaryText="Never" />
-            <MenuItem value={1}  primaryText="Every Night" />
-            <MenuItem value={2}  primaryText="Weeknights" />
+            <MenuItem value={1} primaryText="Every Night" />
+            <MenuItem value={2} primaryText="Weeknights" />
           </SelectField>
-          <SelectField floatingLabelText="Discipline" value={this.state.discipline} onChange={this.handleDisciplineChange}>
+          <SelectField
+            floatingLabelText="Discipline"
+            value={this.state.discipline}
+            onChange={this.handleDisciplineChange}
+          >
             <MenuItem value={0} primaryText="Never" />
-            <MenuItem value={1}  primaryText="Every Night" />
-            <MenuItem value={2}  primaryText="Weeknights" />
+            <MenuItem value={1} primaryText="Every Night" />
+            <MenuItem value={2} primaryText="Weeknights" />
           </SelectField>
           <Divider />
           Biographie :
