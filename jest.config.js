f83fa0e767dfdc0678d8404923198a735a83664f
vincent <vincent.poupet@gmail.com>
2018-02-07 12:27:39 +0100

    fix(test) : add report file

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,5 @@
 module.exports = {
-  coveragePathIgnorePatterns: ['node_modules', '**/docker/**', '**/build/**'],
+  coveragePathIgnorePatterns: ['node_modules', 'api/docker/*','web/docker/*', 'api/build/*','web/build/*'],
+  testResultsProcessor: "jest-sonar-reporter",
+  collectCoverage: true
 }
