62f9929f914bf0e4cf3cbb2071d6b6e37d4dba03
vincent <vincent.poupet@gmail.com>
2018-02-12 22:36:43 +0100

    chore(log) : change log path

--- api/src/utils/logger.js
+++ api/src/utils/logger.js
@@ -5,12 +5,12 @@ import winston from 'winston';
 let transports = [
   new winston.transports.File({
     name: 'ERROR-FILE',
-    filename: 'api-error.log',
+    filename: '/var/log/studim-api-error.log',
     level: 'error',
   }),
   new winston.transports.File({
     name: 'ALL-FILE',
-    filename: 'api-combined.log',
+    filename: '/var/log/studium-combined.log',
   }),
   new winston.transports.Console({ prettyPrint: true, showLevel: 'debug' }),
 ];
