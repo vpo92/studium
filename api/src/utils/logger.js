fcaf0aedc1e6750b7c126a9c548f4ebdc130cc81
vincent <vincent.poupet@gmail.com>
2018-02-17 08:06:31 +0100

    chore(log):remove log file

--- api/src/utils/logger.js
+++ api/src/utils/logger.js
@@ -3,15 +3,6 @@
 import winston from 'winston';
 
 let transports = [
-  new winston.transports.File({
-    name: 'ERROR-FILE',
-    filename: 'studim-api-error.log',
-    level: 'error',
-  }),
-  new winston.transports.File({
-    name: 'ALL-FILE',
-    filename: 'studium-combined.log',
-  }),
   new winston.transports.Console({ prettyPrint: true, showLevel: 'debug' }),
 ];
 
