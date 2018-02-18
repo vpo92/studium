3011269e90572a42082caa8410b02f018b02564a
Arthur Sudre <arthursudre@gmail.com>
2018-02-19 00:12:40 +0100

    fix(web): fix tests for promises on mongo importer

--- batchs/tests/mongo.importer.spec.js
+++ batchs/tests/mongo.importer.spec.js
@@ -2,20 +2,15 @@ import MongoImporter from '../src/mongo.importer';
 
 describe('mongo.importer', () => {
   describe('mongo.importer.importProsopography', () => {
-    it('should raise error if no db', function(done) {
-      MongoImporter.importProsopography(null, {})
-        .then(() => {
-          fail('should have raise error');
-        })
-        .catch(done());
+    it('should raise error if no db', () => {
+      expect(MongoImporter.importProsopography(null, {})).rejects.toEqual(
+        'missing params'
+      );
     });
-
-    it('should raise error if no item', function(done) {
-      MongoImporter.importProsopography({}, null)
-        .then(() => {
-          fail('should have raise error');
-        })
-        .catch(done());
+    it('should raise error if no item', () => {
+      expect(MongoImporter.importProsopography({}, null)).rejects.toEqual(
+        'missing params'
+      );
     });
   });
 });
