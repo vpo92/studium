64ff5c552501a756b9452923e1afe028d883c109
vincent <vincent.poupet@gmail.com>
2019-04-04 20:19:39 +0200

    feat(api) : docs route

--- api/src/routes/routes.js
+++ api/src/routes/routes.js
@@ -1,6 +1,9 @@
 // @flow
 
+
 import express from 'express';
+const path = require('path');
+
 import auth from '../services/authService';
 auth.setup();
 
@@ -8,12 +11,16 @@ import prosopographyRoutes from './prosopographyRoutes';
 import authRoutes from './authRoutes';
 import userRoutes from './userRoutes';
 
+const pkg = require("../../package.json");
 const router = express.Router();
 router.use('/prosopography', prosopographyRoutes);
 router.use('/user', userRoutes);
 router.use('/auth', authRoutes);
+router.get('/docs', (req,res) => {
+    res.sendFile(path.join(__dirname+"../../../docs/index.html"));
+  });
 router.use('/', (req,res) => {
-    res.send({"message":"welcome to studium API", "version":"0.0.3"});
+    res.send({"message":"welcome to studium API", "version":pkg.version});
   });
 
 
