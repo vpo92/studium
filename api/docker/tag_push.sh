83db47fad01c4436df2c9432c3ed852760bb4326
vincent <vincent.poupet@gmail.com>
2019-06-06 11:56:32 +0200

    chore(api) add docker tag version

--- api/docker/tag_push.sh
+++ api/docker/tag_push.sh
@@ -1,17 +1,25 @@
 #!/usr/bin/env bash
 
+PACKAGE_VERSION=$(cat package.json \
+  | grep version \
+  | head -1 \
+  | awk -F: '{ print $2 }' \
+  | sed 's/[",]//g')
+
 DATE=`date +%Y%m%d_%H%M%S`
 
 # LOGIN
 docker login -u ${DOCKER_HUB_LOGIN} -p ${DOCKER_HUB_PASSWORD}
 
 # TAG
-docker tag studium/api:latest studium/api/latest
-docker tag studium/api:latest studium/api/${DATE}
+docker tag studium/api:latest studium/api:latest
+docker tag studium/api:latest studium/api:${PACKAGE_VERSION}
+docker tag studium/api:latest studium/api:${DATE}
 
 # PUSH
-docker push studium/api:${DATE}
 docker push studium/api:latest
+docker push studium/api:${PACKAGE_VERSION}
+docker push studium/api:${DATE}
 
 # LOGOUT
 docker logout
