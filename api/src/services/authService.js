f868c5b994d3b7f9b2a43c17f1e6fc3026bd3c89
vincent <vincent.poupet@gmail.com>
2019-06-06 09:29:53 +0200

    feat(api) extend session time to 4h

--- api/src/services/authService.js
+++ api/src/services/authService.js
@@ -43,7 +43,7 @@ const setup = () => {
   logger.info('authService.setup');
   let opts = {}
   opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
-  opts.secretOrKey = config.auth.secrets.session;;
+  opts.secretOrKey = config.auth.secrets.session;
   //opts.issuer = 'accounts.examplesoft.com';
   //opts.audience = 'yoursite.net';
   passport.use(new JwtStrategy(opts, function(jwt_payload, done) {
