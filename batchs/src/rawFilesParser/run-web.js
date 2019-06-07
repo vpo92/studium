6cec83bf706b81af84db52103e5ec20751520fde
vincent <vincent.poupet@gmail.com>
2019-06-07 07:43:55 +0200

    feat(batch) add local apiUrl

--- batchs/src/rawFilesParser/run-web.js
+++ batchs/src/rawFilesParser/run-web.js
@@ -3,8 +3,9 @@
 import { processFile } from './parser';
 import { saveRecord, createIndex } from './RestService';
 
+//let apiUrl = 'http://localhost:3000';
 let apiUrl = 'http://studium-api.vincentpoupet.fr';
-let token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InZpbmNlbnQucG91cGV0QGdtYWlsLmNvbSIsImlhdCI6MTU1MjY2MDI0OSwiZXhwIjoxNTUyNjYwMzY5fQ.7wFI5GnCuymIBtQoSrJQeE8ghBs1JaFPPjnYeLFxgLQ';
+let token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InZpbmNlbnQucG91cGV0QGdtYWlsLmNvbSIsImlhdCI6MTU1OTg1NDU0MSwiZXhwIjoxNTU5ODY4OTQxfQ.G5dU2Jbz4BcoWK6eCMyCvq0YOETUx2r4GngmjH-f2yk';
 
 let localSaveRecord = (record) => {
   return saveRecord(apiUrl, token, record);
@@ -14,9 +15,9 @@ let localCreateIndex = () => {
 };
 
 //Run import
-processFile('./tests/data/studium_input.txt', localSaveRecord)
+//processFile('./tests/data/studium_input.txt', localSaveRecord)
 //processFile('./tests/data/studium_input_full.txt', localSaveRecord)
-//processFile('../data/full_db.txt', localSaveRecord)
+processFile('../data/full_db.txt', localSaveRecord)
   .then(function(){
     localCreateIndex()
   })
