513885be759ae98a74f50bc17b4b5920302c6df0
vincent <vincent.poupet@gmail.com>
2019-06-18 14:57:24 +0200

    feat(api) add reference data files

--- api/src/routes/routes.js
+++ api/src/routes/routes.js
@@ -19,6 +19,11 @@ router.use('/auth', authRoutes);
 router.get('/docs', (req,res) => {
     res.sendFile(path.join(__dirname+"../../../docs/index.html"));
   });
+router.get('/data/:file', (req,res) => {
+  let file = req.params.file+'.json';
+  console.log(req.params.file);
+  res.sendFile(path.join(__dirname+"../../../resources/json/"+file));
+});
 router.use('/', (req,res) => {
     res.send({"message":"welcome to studium API", "version":pkg.version});
   });
