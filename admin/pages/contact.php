5e62faacf5322bb40b106b2f6a9ccde804dc0106
vincent <vincent.poupet@gmail.com>
2019-07-03 15:35:09 +0200

    chore(admin) :

--- admin/pages/contact.php
+++ admin/pages/contact.php
@@ -3,7 +3,7 @@
 <ul>
     <li>Directeurs du projet : Jean-Philippe GENET et Thierry KOUAME.</li>
     <li>Collaborateurs : Stéphane LAMASSE, Elisabeth MORNET, Pierre-Henri BILLY, Georges-Xavier BLARY.</li>
-    <li>Développement informatique: Hicham IDABAL.</li>
+    <li>Développement informatique: Arthur Sudre & Vincent POUPET</li>
     <li>Ingénieurs de saisie : Claire PRIOL et Anne TOURNIEROUX.</li>
 </ul>
 <h3>Contact</h3>
