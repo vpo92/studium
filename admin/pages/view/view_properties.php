e40f25fff93e2641201017aa290362ace98f9d97
vincent <vincent.poupet@gmail.com>
2019-09-16 12:12:46 +0200

    feat(admin) show opus

--- admin/pages/view/view_properties.php
+++ admin/pages/view/view_properties.php
@@ -6,17 +6,48 @@
  * Time: 08:58
  */
 
-function drawData($data){
-
+function drawReference($refList){
+    echo "<ul>";
+    foreach($refList as $ref){
+        echo "<li class=\"app-reference\">Référence : $ref</li>";
+    }
+    echo "</ul>";
 }
+
+function drawOpus($opusList){
+    $id = uniqid();
+    $i = 0;
+    echo "<ul>";
+    foreach($opusList as $opus){
+        echo "<li>";
+        echo "<a class=\"app-opusCollapseLink\" data-toggle=\"collapse\" href=\"#collapseOpus$id$i\">".$opus->mainTitle."</a>";
+        echo "<div id=\"collapseOpus$id$i\" class=\"collapse\">";
+        echo "<ul>";
+        foreach($opus as $k => $v){
+            drawProperties($opus);
+            //echo "<li><b>".MessageUtils::getMessage('fr',$k).":</b> $v";
+        }
+        echo "</ul>";
+        echo "</div>";
+        echo "</li>";
+        $i++;
+    }
+    echo "</ul>";
+}
+
 
 function drawProperties($prop){
     foreach($prop as $k => $v){
         echo "<li><b>".MessageUtils::getMessage('fr',$k).":</b> ";
+
         if(is_object($v)){
 
             echo $v->value;
 
+            //REFERENCE
+            if(isset($v->reference)){
+                drawReference($v->reference);
+            }
 
 
             //If multiple value
@@ -24,6 +55,17 @@ function drawProperties($prop){
             echo "<ul>";
             foreach($v as $item){
                 echo "<li>$item->value</li>";
+
+                //OPUS
+                if(isset($item->opus)){
+                    drawOpus($item->opus);
+                }
+
+                //REFERENCE
+
+                if(isset($item->reference)){
+                    drawReference($item->reference);
+                }
             }
             echo "</ul>";
         }
@@ -35,7 +77,7 @@ function drawProperties($prop){
 function drawBlock($name,$block){
 
     echo "<h5>".MessageUtils::getMessage('fr',$name)."</h5>";
-    if(is_object($block)){
+    if(is_object($block) || is_array($block)){
         echo "<ul>";
         drawProperties($block);
         echo "</ul>";
