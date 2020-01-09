0f946300a4e42bb440eb2cc6d63d84cd5dcdc1e3
vincent <vincent.poupet@gmail.com>
2020-01-09 23:30:40 +0100

    fix(admin) fix applicationUrl

--- admin/pages/view.php
+++ admin/pages/view.php
@@ -3,7 +3,7 @@
 ?>
 
 <h3>Fiche <?php echo $fiche->reference ?> - <?php echo $fiche->title ?></h3>
-<h6>permalien : <a href="<?php echo getPublicAPIUrl().$fiche->link ?>"><?php echo getPublicAPIUrl().$fiche->link ?></a></h6>
+<h6>permalien : <a href="<?php echo getApplicationUrl().$fiche->link ?>"><?php echo getApplicationUrl().$fiche->link ?></a></h6>
 
 <?php
 $name = is_object($fiche->identity->name)?$fiche->identity->name->value:$fiche->identity->name[0]->value;
