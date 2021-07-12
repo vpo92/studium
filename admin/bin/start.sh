a9e090377c25f507d3d3bc3af238446feaae869d
vincent <vincent.poupet@gmail.com>
2021-07-12 10:39:34 +0200

    feat(admin) fix redirect routes error in local mode

--- admin/bin/start.sh
+++ admin/bin/start.sh
@@ -2,4 +2,4 @@
 
 export APPLICATION_URL="http://localhost:8000"
 open http://localhost:8000
-php -S localhost:8000
+php -S localhost:8000 router.php
---
+++ admin/router.php
@@ -0,0 +1,30 @@
+<?php
+$request = $_SERVER['REQUEST_URI'];
+
+//si le fichier existe, on le sert
+if(file_exists(dirname(__FILE__).$request) && is_file(dirname(__FILE__).$request)){
+    $ext = substr($request, strripos($request, '.')+1);
+    switch($ext){
+      case 'css':
+        header('Content-Type:text/css');
+        break;
+      case 'js':
+        header('Content-Type:application/javascript');
+        break;
+      case 'jpg':
+        header('Content-Type:image/jpeg');
+        break;
+      case 'png':
+        header('Content-Type:image/png');
+        break;
+        break;
+    }
+
+
+    readfile(dirname(__FILE__).$request);
+    exit;
+}else{
+  include "index.php";
+}
+
+?>
