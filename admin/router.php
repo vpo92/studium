236f591eead70f8a8154e66e8320569ae302c703
vincent <vincent.poupet@gmail.com>
2021-07-12 11:16:26 +0200

    fix(admin) route to index.php

--- admin/router.php
+++ admin/router.php
@@ -2,7 +2,7 @@
 $request = $_SERVER['REQUEST_URI'];
 
 //si le fichier existe, on le sert
-if(file_exists(dirname(__FILE__).$request) && is_file(dirname(__FILE__).$request)){
+if(file_exists(dirname(__FILE__).$request) && is_file(dirname(__FILE__).$request) && $request != "/index.php"){
     $ext = substr($request, strripos($request, '.')+1);
     switch($ext){
       case 'css':
