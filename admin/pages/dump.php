2e96de09a4aaf8a7cb7ec110289926096477df4b
VPO92 <vpo92@users.noreply.github.com>
2023-02-06 17:04:59 +0100

    feat(admin) dump n/n

--- admin/pages/dump.php
+++ admin/pages/dump.php
@@ -7,10 +7,10 @@ $pageScripts .='<script src="https://cdn.jsdelivr.net/npm/vue@2/dist/vue.js"></s
 $pageScripts .='<script src="https://unpkg.com/bootstrap-vue@latest/dist/bootstrap-vue.js"></script>';
 $pageScripts .='<script src="'.getResourcesWebDirectory().'/js/dump.js"></script>';
 ?>
-<h3>Gestion des Dumps</h3>
+<h3>Gestion des backups</h3>
 <div id="app" v-cloak>
 
-    <h4>Gestion des backups</h4>
+    <h4>Historique</h4>
 
     <button class="btn btn-primary" @click="list()">Rafraichir</button>
     <button class="btn btn-danger" @click="new()">Créer un dump</button>
